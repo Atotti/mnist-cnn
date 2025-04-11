0c1c31772085345023fcd20065b447ef21072795
Ayuto <56724676+Atotti@users.noreply.github.com>
2025-04-12 01:05:28 +0900

    resnet34

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def main() -> None:
     parser.add_argument(
         "--test-batch-size", type=int, default=1000, metavar="N", help="input batch size for testing (default: 1000)"
     )
-    parser.add_argument("--epochs", type=int, default=14, metavar="N", help="number of epochs to train (default: 14)")
+    parser.add_argument("--epochs", type=int, default=20, metavar="N", help="number of epochs to train (default: 14)")
     parser.add_argument("--lr", type=float, default=1.0, metavar="LR", help="learning rate (default: 1.0)")
     parser.add_argument("--gamma", type=float, default=0.7, metavar="M", help="Learning rate step gamma (default: 0.7)")
     parser.add_argument("--no-cuda", action="store_true", default=False, help="disables CUDA training")
@@ -67,7 +67,7 @@ def main() -> None:
     )
 
     # Create model
-    model = torchvision.models.resnet50(pretrained=True)
+    model = torchvision.models.resnet34()
     model.fc = torch.nn.Sequential(
         torch.nn.Linear(model.fc.in_features, 10),
         torch.nn.LogSoftmax(dim=1)
