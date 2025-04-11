ae4206f3850cd7439273f631debafa07edc10dc2
Ayuto <56724676+Atotti@users.noreply.github.com>
2025-04-12 01:24:06 +0900

    ResNet18

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ def main() -> None:
     )
 
     # Create model
-    model = torchvision.models.resnet34()
+    model = torchvision.models.resnet18()
     model.fc = torch.nn.Sequential(
         torch.nn.Linear(model.fc.in_features, 10),
         torch.nn.LogSoftmax(dim=1)
